a482ead90297e20da2dd3c90bbd0f09a83052eb8
artemisia <oggiano.chiara@gmail.com>
2015-12-12 12:26:59 +0100

    creato template post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+#return un metodo render che ci fornirà il template blog/post_list.html
+def post_list(request):
+	return render(request, 'blog/post_list.html', {})
+
