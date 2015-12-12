113cc3229aa1b2196f5fa4e8a30f02a86c14cd36
artemisia <oggiano.chiara@gmail.com>
2015-12-12 12:56:20 +0100

    template modificato

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 #return un metodo render che ci fornirà il template blog/post_list.html
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts':posts})
 
